eff1c6cd4bcdafe09065f020ef488c30dcad5b4c
sekhar Reddy <sekharsekhar1919@gmail.com>
2026-08-13 13:12:59 +0530

    Prepared frontend for production deployment & dynamic server URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,6 @@
-let IS_PROD = false;
-const server = IS_PROD ?
-    "https://apnavideocallbackend.onrender.com" :
-    "http://localhost:8000"
-
+const server = import.meta.env.VITE_SERVER_URL || 
+    (import.meta.env.PROD 
+        ? "https://novacall-backend.onrender.com" 
+        : "http://localhost:8000");
 
 export default server;
